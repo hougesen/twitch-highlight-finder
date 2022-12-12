75093750b30dead8d0497183de3762423fdda12b
Mads Hougesen <madshougesen@gmail.com>
2022-12-12 21:25:43 +0100

    feat(analyzer): base implementation of chat analyzer

---
+++ analyzer/src/analyzer/mod.rs
@@ -0,0 +1,103 @@
+pub fn tokenize_message(
+    message: &str,
+    emote_scores: &dashmap::ReadOnlyView<String, u8>,
+) -> Vec<(String, u8)> {
+    let mut tokens = Vec::new();
+
+    for word in message.split_whitespace() {
+        if !word.is_empty() && !word.starts_with('@') {
+            tokens.push((word.to_string(), *emote_scores.get(word).unwrap_or(&0u8)));
+        }
+    }
+
+    tokens
+}
+
+pub fn calculate_message_score(tokens: &Vec<(String, u8)>) -> u8 {
+    let mut modifier = 0;
+
+    for (_token, score) in tokens {
+        if *score > modifier {
+            modifier = *score;
+        }
+    }
+
+    100 + modifier
+}
+
+pub struct AnalysedMessage {
+    pub message: String,
+    pub tokens: Vec<(String, u8)>,
+    pub message_score: u8,
+}
+
+pub fn analyze_message(
+    message: String,
+    emote_scores: &dashmap::ReadOnlyView<String, u8>,
+) -> AnalysedMessage {
+    let tokens = tokenize_message(&message, emote_scores);
+
+    AnalysedMessage {
+        message,
+        message_score: calculate_message_score(&tokens),
+        tokens,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::calculate_message_score;
+
+    #[test]
+    fn test_tokenize_message() {
+        use dashmap::DashMap;
+
+        use crate::scores::tokenize_message;
+
+        let emote_scores = DashMap::new();
+
+        emote_scores.insert("Kappa".to_string(), 8u8);
+        emote_scores.insert("PogChamp".to_string(), 10u8);
+
+        let locked_map = emote_scores.into_read_only();
+
+        // Should set non emotes to 0
+        assert_eq!(
+            vec![
+                ("string".to_string(), 0),
+                ("without".to_string(), 0),
+                ("emote".to_string(), 0),
+            ],
+            tokenize_message("   string    without    emote   ", &locked_map)
+        );
+
+        // should ignore mentions (@)
+        assert_eq!(
+            vec![("hello".to_owned(), 0)],
+            tokenize_message("@hougesen hello", &locked_map)
+        );
+
+        // should return score if word is an emote
+        assert_eq!(
+            vec![
+                ("Kappa".to_string(), 8),
+                ("Kappa".to_string(), 8),
+                ("Kappa".to_string(), 8)
+            ],
+            tokenize_message("Kappa Kappa Kappa ", &locked_map)
+        );
+
+        assert_eq!(
+            vec![("PogChamp".to_string(), 10), ("Kappa".to_string(), 8)],
+            tokenize_message("PogChamp  Kappa ", &locked_map)
+        );
+    }
+
+    #[test]
+    fn test_calculate_message_score() {
+        assert_eq!(
+            100,
+            calculate_message_score(&vec![("without".to_string(), 0)])
+        )
+    }
+}
---
+++ analyzer/src/db/mod.rs
@@ -0,0 +1,122 @@
+use dashmap::DashMap;
+use futures::stream::TryStreamExt;
+use mongodb::{
+    bson::{doc, oid::ObjectId},
+    Database,
+};
+use serde::Deserialize;
+
+pub async fn get_db_client() -> Result<Database, mongodb::error::Error> {
+    let db_connection_string =
+        dotenv::var("MONGO_CONNECTION_STRING").expect("Missing env MONGO_CONNECTION_STRING");
+
+    let client = mongodb::Client::with_uri_str(db_connection_string).await?;
+
+    Ok(client.database("highlights"))
+}
+
+#[derive(Debug, Deserialize)]
+pub struct TwitchEmote {
+    #[serde(rename = "_id")]
+    pub _id: ObjectId,
+    pub emote_id: String,
+    /// The name of the emote. This is the name that viewers type in the chat window to get the emote to appear.
+    pub name: String,
+    pub channel_id: Option<String>,
+    pub score: Option<u8>,
+}
+
+pub async fn get_emote_scores(db_client: &Database) -> DashMap<String, u8> {
+    let collection = db_client.collection::<TwitchEmote>("emotes");
+
+    let emote_scores: DashMap<String, u8> = DashMap::new();
+
+    // TODO: convert to aggregate to reduce data size?
+    if let Ok(cursor) = collection.find(None, None).await {
+        if let Ok(emotes) = cursor.try_collect::<Vec<TwitchEmote>>().await {
+            for emote in emotes {
+                emote_scores.insert(emote.name, emote.score.unwrap_or(1));
+            }
+        };
+    }
+
+    emote_scores
+}
+
+#[derive(Deserialize)]
+pub struct TwitchChatMessage {
+    #[serde(rename = "_id")]
+    pub id: ObjectId,
+    pub channel: String,
+    pub sender: String,
+    pub message: String,
+    pub timestamp: mongodb::bson::DateTime,
+    pub message_score: Option<f32>,
+}
+
+#[derive(Deserialize)]
+pub struct FoundMessage {
+    #[serde(rename = "_id")]
+    pub id: ObjectId,
+    pub message: String,
+}
+
+pub async fn get_pending_chat_messages(db_client: &Database) -> Vec<FoundMessage> {
+    let collection = db_client.collection::<TwitchChatMessage>("twitch_messages");
+
+    let cursor = collection
+        .aggregate(
+            vec![
+                doc! {
+                    "$match": {
+                        "message_score": {
+                            "$exists": false
+                        }
+                    }
+                },
+                doc! {
+                    "$project": {
+                        "_id": 1,
+                        "message": 1
+                    }
+                },
+            ],
+            None,
+        )
+        .await;
+
+    if let Ok(cursor) = cursor {
+        let messages = cursor
+            .with_type::<FoundMessage>()
+            .try_collect::<Vec<FoundMessage>>()
+            .await;
+
+        if let Ok(messages) = messages {
+            return messages;
+        }
+    }
+
+    vec![]
+}
+
+pub async fn save_message_score(
+    db_client: Database,
+    message_id: ObjectId,
+    message_score: u8,
+) -> Result<mongodb::results::UpdateResult, mongodb::error::Error> {
+    let collection = db_client.collection::<TwitchChatMessage>("twitch_messages");
+
+    collection
+        .update_one(
+            doc! {
+                "_id": message_id
+            },
+            doc! {
+                "$set": {
+                    "message_score": f64::from(message_score) / 100.0
+                }
+            },
+            None,
+        )
+        .await
+}
