1f10a06515b41b6021352c21eec543d671b99154
Mads Hougesen <madshougesen@gmail.com>
2022-12-18 18:39:27 +0100

    refactor(chatbot): convert to async (#20)

--- chatbot/src/db.rs
+++ chatbot/src/db.rs
@@ -1,14 +1,15 @@
 use mongodb::bson::oid::ObjectId;
-use mongodb::sync::{Client, Database};
+use mongodb::{Client, Database};
 use tungstenite::Message;
 
-pub fn get_db_client() -> Database {
+pub async fn get_db_client() -> Result<Database, mongodb::error::Error> {
     let db_connection_string =
         dotenv::var("MONGO_CONNECTION_STRING").expect("Missing env MONGO_CONNECTION_STRING");
 
-    let client = Client::with_uri_str(db_connection_string).unwrap();
-
-    client.database("highlights")
+    match Client::with_uri_str(db_connection_string).await {
+        Ok(client) => Ok(client.database("highlights")),
+        Err(mongo_error) => Err(mongo_error),
+    }
 }
 
 struct ChannelCollection {
@@ -17,12 +18,12 @@ struct ChannelCollection {
     channel_name: String,
 }
 
-pub fn get_channel_queue(db: &Database) -> Vec<Message> {
+pub async fn get_channel_queue(db: &Database) -> Vec<Message> {
     let collection = db.collection::<ChannelCollection>("channels");
 
     let mut channel_queue = Vec::new();
 
-    if let Ok(channel_queue_bson) = collection.distinct("channel_name", None, None) {
+    if let Ok(channel_queue_bson) = collection.distinct("channel_name", None, None).await {
         if !channel_queue_bson.is_empty() {
             for channel in channel_queue_bson {
                 if let Some(channel_name) = channel.as_str() {
