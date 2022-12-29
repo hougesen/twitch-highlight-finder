37f81ed6a6f0ecd61d4c7e2cf7ab3df2583a0a30
Mads Hougesen <madshougesen@gmail.com>
2022-12-29 19:55:20 +0100

    refactor(chatbot): split message with multiple lines into multiple items

--- chatbot/src/queue/mod.rs
+++ chatbot/src/queue/mod.rs
@@ -1,6 +1,7 @@
 use async_channel::Receiver;
 use aws_sdk_sqs as sqs;
 use mongodb::bson::DateTime;
+use sqs::{error::SendMessageError, output::SendMessageOutput, types::SdkError};
 
 pub async fn setup_sqs_client() -> sqs::Client {
     let config = aws_config::load_from_env().await;
@@ -38,21 +39,41 @@ pub async fn message_queuer(
     let queue_url = queue.queue_url().unwrap();
 
     while !message_rx.is_closed() {
-        if let Ok((message, timestamp)) = message_rx.recv().await {
-            if message.contains("PRIVMSG") {
-                sqs_client
-                    .send_message()
-                    .queue_url(queue_url)
-                    .message_body(
-                        serde_json::to_string(&QueueMessage { message, timestamp }).unwrap(),
-                    )
-                    .send()
-                    .await?;
-            } else {
-                println!("UNKNOWN MESSAGE: {:?}", message.trim());
+        if let Ok((original_message, timestamp)) = message_rx.recv().await {
+            let trimmed_message = original_message.trim();
+
+            for message in trimmed_message.split("\r\n").collect::<Vec<&str>>() {
+                if message.contains("PRIVMSG") {
+                    queue_message(&sqs_client, queue_url, message.trim(), timestamp.clone()).await;
+                } else {
+                    eprintln!("UNKNOWN MESSAGE: {message}");
+                }
             }
         }
     }
 
+    eprintln!("outside messageq_queuer");
+
     Ok(())
+}
+
+#[inline]
+async fn queue_message(
+    sqs_client: &aws_sdk_sqs::Client,
+    queue_url: &str,
+    message: &str,
+    timestamp: DateTime,
+) -> Result<SendMessageOutput, SdkError<SendMessageError>> {
+    sqs_client
+        .send_message()
+        .queue_url(queue_url)
+        .message_body(
+            serde_json::to_string(&QueueMessage {
+                message: message.to_string(),
+                timestamp,
+            })
+            .unwrap(),
+        )
+        .send()
+        .await
 }
