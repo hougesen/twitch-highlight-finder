1edeb79e1d56331127c4dc1a480124bbcfe693fb
Mads Hougesen <madshougesen@gmail.com>
2023-01-02 00:16:32 +0100

    refactor(highlight-finder): ignore buckets with less than 1 msg/s

--- highlight-finder/src/finder/mod.rs
+++ highlight-finder/src/finder/mod.rs
@@ -21,14 +21,14 @@ pub async fn analyze_vod(
 
     let bucket = create_bucket(scores);
 
-    let average = calculate_bucket_average(&bucket);
+    let average = calculate_weighted_bucket_average(&bucket);
 
     let outliers = find_outlier_timestamps(&bucket, average);
 
     Ok(create_clips(outliers, vod))
 }
 
-const TIME_BUCKET_SIZE_MS: i64 = 10_000;
+const TIME_BUCKET_SIZE_MS: u16 = 10_000;
 
 /// TODO: switch to sliding window?
 fn create_bucket(message_scores: impl IntoIterator<Item = VodMessageScore>) -> HashMap<i64, f64> {
@@ -36,7 +36,7 @@ fn create_bucket(message_scores: impl IntoIterator<Item = VodMessageScore>) -> H
 
     for score in message_scores {
         let timestamp_ms = score.timestamp.timestamp_millis();
-        let key = timestamp_ms - (timestamp_ms % TIME_BUCKET_SIZE_MS);
+        let key = timestamp_ms - (timestamp_ms % i64::from(TIME_BUCKET_SIZE_MS));
 
         bucket
             .entry(key)
@@ -56,11 +56,14 @@ fn find_outlier_timestamps(original_bucket: &HashMap<i64, f64>, average: f64) ->
 
     values.sort_by(|a, b| a.total_cmp(b));
 
-    let top_10_percentage = values[(values.len() / 10) * 9] as i64;
-    let average_modifier = (average * 1.5) as i64;
+    let top_5_percentage = values[(values.len() * 95) / 100] as i64;
+
     let base = TIME_BUCKET_SIZE_MS / 1000;
 
-    let min = std::cmp::max(top_10_percentage, std::cmp::max(average_modifier, base)) as f64;
+    let min = (std::cmp::max(
+        top_5_percentage,
+        std::cmp::max(average as i64, i64::from(base)),
+    ) * 2) as f64;
 
     let mut outliers = BTreeSet::new();
 
@@ -73,14 +76,23 @@ fn find_outlier_timestamps(original_bucket: &HashMap<i64, f64>, average: f64) ->
     outliers
 }
 
-fn calculate_bucket_average(bucket: &HashMap<i64, f64>) -> f64 {
+/// Psuedo
+fn calculate_weighted_bucket_average(bucket: &HashMap<i64, f64>) -> f64 {
     let mut total = 0.;
+    let mut count = 0;
+
+    // 1 message per second
+    let min = f64::from(TIME_BUCKET_SIZE_MS) / 1000.;
 
     for s in bucket.values() {
-        total += s;
+        // ignore any bucket that has less than 1 message per second, since it makes the numbers unreliable
+        if s >= &min {
+            total += s;
+            count += 1;
+        }
     }
 
-    total / bucket.len() as f64
+    total / f64::from(count)
 }
 
 #[inline]
@@ -95,8 +107,8 @@ fn create_clips(mut timestamps: BTreeSet<i64>, vod: &TwitchVodModel) -> Vec<Clip
         if let Some(start_time) = timestamps.pop_first() {
             let mut end_time = start_time;
 
-            while timestamps.contains(&(end_time + TIME_BUCKET_SIZE_MS)) {
-                end_time += TIME_BUCKET_SIZE_MS;
+            while timestamps.contains(&(end_time + i64::from(TIME_BUCKET_SIZE_MS))) {
+                end_time += i64::from(TIME_BUCKET_SIZE_MS);
 
                 timestamps.remove(&end_time);
             }
@@ -121,7 +133,7 @@ mod tests {
 
     use crate::db::twitch_messages::VodMessageScore;
 
-    use super::{calculate_bucket_average, create_bucket};
+    use super::{calculate_weighted_bucket_average, create_bucket};
 
     #[test]
     fn test_create_bucket() {
@@ -188,12 +200,16 @@ mod tests {
     fn test_calculate_bucket_average_score() {
         assert_eq!(
             4.0 / 3.0,
-            calculate_bucket_average(&HashMap::from([(0, 1.0), (5000, 2.0), (10000, 1.0)]))
+            calculate_weighted_bucket_average(&HashMap::from([
+                (0, 1.0),
+                (5000, 2.0),
+                (10000, 1.0)
+            ]))
         );
 
         assert_eq!(
             (123.12 + 21.121 + 512.12) / 3.0,
-            calculate_bucket_average(&HashMap::from([
+            calculate_weighted_bucket_average(&HashMap::from([
                 (0, 123.12),
                 (5000, 21.121),
                 (10000, 512.12)
