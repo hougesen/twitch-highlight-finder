e9df232cf411dfc37ee9743cdc25143a7610cd94
Mads Hougesen <madshougesen@gmail.com>
2023-01-18 19:23:09 +0100

    refactor: switch to aws lambda framework

--- live-tracker/src/index.ts
+++ live-tracker/src/index.ts
@@ -1,37 +1,68 @@
-import 'dotenv/config';
+import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import {
+    MESSAGE_SECRET,
+    HMAC_PREFIX,
+    TWITCH_MESSAGE_SIGNATURE,
+    MESSAGE_TYPE_VERIFICATION,
+    MESSAGE_TYPE,
+    MESSAGE_TYPE_REVOCATION,
+    MESSAGE_TYPE_NOTIFICATION,
+} from './config';
+import { queueMessage } from './queue';
+import { TwitchNotificationChallenge, TwitchNotificationStreamOnline, TwitchNotificationStreamOffline } from './types';
+import { getHmacMessage, getHmac, verifyMessage } from './verification';
 
-import http from 'http';
+export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+    console.log(`Event: ${JSON.stringify(event, null, 2)}`);
 
-import { handleRequest } from './response';
-import { Request, TwitchNotification } from './types';
+    console.log(`Context: ${JSON.stringify(context, null, 2)}`);
 
-function parseRequestBody(bodyStr: string): TwitchNotification | undefined {
     try {
-        return JSON.parse(bodyStr);
-    } catch {
-        return undefined;
+        const message = getHmacMessage(event?.headers, event?.body);
+
+        if (!MESSAGE_SECRET) {
+            return { statusCode: 500, body: '' };
+        }
+
+        const hmac = HMAC_PREFIX + getHmac(MESSAGE_SECRET, message);
+
+        const validMessage = verifyMessage(hmac, event?.headers?.[TWITCH_MESSAGE_SIGNATURE] ?? '');
+
+        if (!validMessage) {
+            return { statusCode: 403, body: '' };
+        }
+
+        if (MESSAGE_TYPE_REVOCATION === event?.headers?.[MESSAGE_TYPE]) {
+            return { statusCode: 204, body: '' };
+        }
+
+        // Challenge
+        if (MESSAGE_TYPE_VERIFICATION === event?.headers?.[MESSAGE_TYPE]) {
+            return {
+                statusCode: 200,
+                body: (JSON.parse(event?.body ?? '{}') as TwitchNotificationChallenge)?.challenge,
+            };
+        }
+
+        if (MESSAGE_TYPE_NOTIFICATION === event?.headers?.[MESSAGE_TYPE]) {
+            if (event?.body) {
+                const body = JSON.parse(event.body) as TwitchNotificationStreamOnline | TwitchNotificationStreamOffline;
+
+                switch (body?.subscription?.type) {
+                    case 'stream.online':
+                    case 'stream.offline':
+                        await queueMessage(body?.subscription?.type, body?.event?.broadcaster_user_login);
+                        break;
+
+                    default:
+                        console.info('Unknown subscription type', body?.subscription?.type);
+                        break;
+                }
+            }
+        }
+
+        return { statusCode: 204, body: '' };
+    } catch (error) {
+        return { statusCode: 400, body: JSON.stringify(error) };
     }
-}
-
-async function handleIncomingRequest(req: Request, res: http.ServerResponse) {
-    const chunks: Uint8Array[] = [];
-
-    req.on('data', (chunk: Uint8Array) => {
-        chunks.push(chunk);
-    }).on('end', () => {
-        req.body_str = Buffer.concat(chunks)?.toString() ?? '';
-        req.body = parseRequestBody(req?.body_str);
-
-        handleRequest(req, res);
-    });
-}
-
-const server = http.createServer();
-
-server.on('request', handleIncomingRequest);
-
-const PORT = process.env.PORT ?? 1234;
-
-server.listen(PORT);
-
-server.on('listening', () => console.info(`Listening on :${PORT}`));
+};
--- live-tracker/src/verification.ts
+++ live-tracker/src/verification.ts
@@ -1,17 +1,19 @@
-import crypto from 'crypto';
-import { IncomingHttpHeaders } from 'http';
-
+import { APIGatewayProxyEventHeaders } from 'aws-lambda';
+import { createHmac, timingSafeEqual } from 'crypto';
 import { TWITCH_MESSAGE_ID, TWITCH_MESSAGE_TIMESTAMP } from './config';
 
-export function getHmacMessage(headers: IncomingHttpHeaders, body?: string) {
-    // @ts-expect-error allow undefined on purpose
-    return headers[TWITCH_MESSAGE_ID] + headers[TWITCH_MESSAGE_TIMESTAMP] + body;
+export function getHmacMessage(headers: APIGatewayProxyEventHeaders, body?: string | null) {
+    const messageId = headers?.[TWITCH_MESSAGE_ID] ?? '';
+
+    const messageTimestamp = headers?.[TWITCH_MESSAGE_TIMESTAMP] ?? '';
+
+    return messageId + messageTimestamp + body;
 }
 
 export function getHmac(secret: string, message: string) {
-    return crypto.createHmac('sha256', secret).update(message).digest('hex');
+    return createHmac('sha256', secret).update(message).digest('hex');
 }
 
 export function verifyMessage(hmac: string, verifySignature: string) {
-    return crypto.timingSafeEqual(Buffer.from(hmac), Buffer.from(verifySignature));
+    return timingSafeEqual(Buffer.from(hmac), Buffer.from(verifySignature));
 }
